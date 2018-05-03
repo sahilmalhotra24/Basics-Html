df20fd5cb59a17107f3904ab1a303ae1ea4b38f5
Sahil Malhotra <sahilmalhotra578@gmail.com>
2018-05-03 07:58:15 +0530

    Create Express.js

---
+++ Web Programming/node/Express.js
@@ -0,0 +1,8 @@
+example 1
+var express = require('express');
+var app = express();
+
+app.get('/things/:name/:id', function(req, res) {
+   res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
+});
+app.listen(3000);
